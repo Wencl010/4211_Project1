7c2fc20c02efeecfeca80a1a4086af88edb9bbc6
Billy Wencl <wencl010@umn.edu>
2022-10-27 11:10:19 -0500

    Implement Basic Socket server

--- Server.cc
+++ Server.cc
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <string> 
+#include <string>
+
+//socket includes
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 
 #include "Topic.h"
 #include "Subscription.h"
+
+#define PORT 4211
+#define QUEUE_LEN 50
 
 std::string printSubs(std::vector<Subscription*> subs, bool printTopics){
     std::string print = "# ";
@@ -26,45 +36,68 @@ int main(){
     TopicManager topicMgr;    
     SubscriptionManager subMgr;
 
-    Topic* weather = topicMgr.getTopic("WEATHER");
-    Topic* news = topicMgr.getTopic("NEWS");
-    Topic* security = topicMgr.getTopic("SECURITY");
-    Topic* health = topicMgr.getTopic("HEALTH");
- 
-    std::cout << weather->getName()<<"\n";
-    std::cout << news->getName()<<"\n";
-    std::cout << security->getName()<<"\n";
-    std::cout << health->getName()<<"\n";
+    int serverFd; 
+    int tempClientFd;
+    std::vector<int> clientFd; //ToDo: consider only using subscriptions to manage these.
 
+    struct sockaddr serverAddr;
+    struct sockaddr clientAddr;
 
+    //Open Socket File Descriptor
+    serverFd = socket(PF_INET, SOCK_STREAM, 0);
+    if(serverFd == -1){
+        perror("Socket error");
+        exit(-1);
+    }
 
-    subMgr.addSubscription(1, security);
-    subMgr.addSubscription(1, security);
-    subMgr.addSubscription(1, health);
+    //Set Socket options to allow port reuse
+    int enable = 1;
+    if(setsockopt(serverFd, SO_REUSEADDR, &enable, sizeof(int)) == -1){
+        perror("Set reuse errors");
+        exit(-1);
+    }
 
-    subMgr.addSubscription(2, health);
-    subMgr.addSubscription(2, news);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(PORT);
+    
+    //Bind server to localhost
+    if(bind(serverFd, &serverAddr, sizeof(serverAddr)) == -1){
+        perror("Bind error");
+        exit(-1)
+    }
 
+    if(listen(serverFd, QUEUE_LEN) == -1){
+        perror("Listen Error");
+        exit(-1);
+    }
 
-    subMgr.addSubscription(4, news);
-    subMgr.addSubscription(4, security);
-    subMgr.addSubscription(4, health);
+    std::cout << "start accepting connections:\n\n";
+    int clientAddrSize = sizeof(clientAddr);
+    std::string msg = "Hello World\n";
 
+    for(int i =0; i<5; i++){
+        tempClientFd = accept(serverFd, &clientAddr, &clientAddrSize);
+        if(tempClientFd == -1){
+            perror("Problem accepting connection");
+            continue; //Jump to next loop and wait for next connection
+        }
 
-    std::cout << printSubs(subMgr.getSubByClient(1), true) <<"\n";
+        std::cout<< i << ") Connection made\n";
+        if(write(tempClientFd, msg, sizeof(msg)) == -1){perror("Write Failed")}
+        
+        clientFd.push_back(tempClientFd);
+    }
+    std::cout << "stop accepting connections.\n";
 
-    std::cout << printSubs(subMgr.getSubByClient(2), true) <<"\n";
+    msg = "Goodbye";
+    for(int i = 0; i<clientFd.size(); i++){
+       if(write(clientFd.at(i), msg, sizeof(msg)) == -1){perror("Write Failed")}
+        close(clientFd.at(i));
+    }
 
-    std::cout << printSubs(subMgr.getSubByClient(3), true) <<"\n";
+    std::cout << "Goodbyes sent.\n";
 
-    std::cout << printSubs(subMgr.getSubByClient(4), true) <<"\n";
-
-    std::cout << printSubs(subMgr.getSubByClient(5), true) <<"\n";
-
-
-    std::cout << printSubs(subMgr.getSubByTopic(news), false) <<"\n";
-    std::cout << printSubs(subMgr.getSubByTopic(weather), false) <<"\n";
-    std::cout << printSubs(subMgr.getSubByTopic(security), false) <<"\n";
-    std::cout << printSubs(subMgr.getSubByTopic(health), false) <<"\n";
+    close(serverAddr);
 
 }
