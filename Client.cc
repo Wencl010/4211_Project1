56b4bab3515fd09875a89c361e9e3791243d4e2d
Billy Wencl <wencl010@umn.edu>
2022-10-29 13:03:23 -0500

    Implement Listening and UI for Client

--- Client.cc
+++ Client.cc
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <unistd.h> 
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <signal.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,14 +11,69 @@
 
 #include "MsgProtocol.h"
 
-#define PORT 4212
+int connectToServer();
+int disconnectFromServer();
+void listenOnServer();
+void endListen(int sig);
+
+#define PORT 4211
+
+struct sigaction action; 
+int serverFd;
+bool listening = false;
+
 
 int main(){
-    int msgSize; 
-    MsgPacket msgPck;
-    MsgPacket msgPck2;
+    serverFd = connectToServer();
+    std::cout<<"Connection Open:\n\n";
+
+    std::string userInput;
+
+    bool run = true;
+    while(run){
+        std::cout << "\nOptions: \nL - listen for messages in subscriptions\nS - subscribe to messages\nP - publish a message\nQ - quit\n";
+        std::cout << "Enter choice: ";
+
+        std::cin >> userInput;
+
+        std::cout << "\n";
+
+        if(userInput == "L"){
+            listenOnServer();
+        }
+        else if(userInput == "S"){
+            
+        }
+        else if(userInput == "P"){
+            
+        }
+        else if(userInput == "Q"){
+            run = false;
+        }
+        else{
+            std::cout << "Invalid option\n";
+        }
 
 
+
+    }
+
+
+    disconnectFromServer();
+
+    std::cout<<"Connection Closed\n";
+    exit(1);
+}
+
+
+
+/**
+ * Sets up the connection between client and server, then sends the connect msg
+ * and waits for the serves response before proceeding.
+ * 
+ * @return the server socket's file descriptor 
+ */
+int connectToServer(){
     //Open Socket File Descriptor
     int socketFd = socket(PF_INET, SOCK_STREAM, 0);
     if(socketFd == -1){
@@ -32,27 +90,82 @@ int main(){
         perror("Connection Error");
         exit(-1);
     }
-    std::cout<<"Connection Open:\n\n";
 
-    msgSize = read(socketFd, &msgPck, sizeof(MsgPacket));
-    if(msgSize == -1){
-        perror("Read Error");
+
+    //exchange connect messages with server
+    MsgPacket connectMsg; 
+    connectMsg.type = MT_Connect;
+    if(write(socketFd, &connectMsg, sizeof(MsgPacket))==-1){perror("Server Connection Failed"); exit(1);}
+
+    //wait for response form server
+    MsgPacket response; 
+    if(read(socketFd, &response, sizeof(MsgPacket))==-1){perror("Server Ack Failed"); exit(1);}
+    //process response from server
+    if(response.type != MT_Conn_ACK){
+        perror("Server Failed to acknowledge connection.");
         exit(-1);
     }
 
-    std::cout << msgSize <<"\n";
-    printMsg(msgPck);
-    std::cout <<"\n";
+    printMsg(response);
 
+    return socketFd;
+}
+ 
+/**
+ * Ends connection with server with proper disconnect messages
+ * 
+ * @param serverFd the server to disconnect from
+ * @return 1 if successfully disconnected 0 otherwise 
+ */
+int disconnectFromServer(){
+    //exchange connect messages with server
+    MsgPacket disconnectMsg; 
+    disconnectMsg.type = MT_Disconnect;
+    if(write(serverFd, &disconnectMsg, sizeof(MsgPacket))==-1){perror("Server Disconnect Failed"); return -1;}
 
-    msgSize = read(socketFd, &msgPck2, sizeof(MsgPacket));
-    if(msgSize == -1){
-        perror("Read Error");
-        exit(-1);
+    //wait for response from server
+    MsgPacket response; 
+    if(read(serverFd, &response, sizeof(MsgPacket))==-1){perror("Server Ack Failed"); return -1;}
+    //process response from server
+    if(response.type != MT_Disc_ACK){
+        perror("Server Failed to acknowledge disconnect.");
+        return -1;
     }
-    std::cout << msgSize <<"\n";
-    printMsg(msgPck2);
-    close(socketFd);
 
-    std::cout<<"Connection Closed\n";
+    printMsg(response);
+    return 1;
+}
+
+void listenOnServer(){
+    //Set "ctrl + c" interrupt to return to exit listening mode
+    action.sa_handler = endListen;
+    sigemptyset(&action.sa_mask);
+    sigaddset(&action.sa_mask, SIGINT);
+    action.sa_flags = 0;
+    sigaction(SIGINT, &action, NULL);
+
+    listening = true;
+
+    MsgPacket serverMsg;
+    while(listening){
+        std::cout << "Waiting for messages...\n";
+        int readSize = read(serverFd, &serverMsg, sizeof(MsgPacket));
+        if(readSize == -1){perror("Write Failed");}
+        std::cout << "Message Received: \n\n";
+
+
+        std::cout << "Topic: " << serverMsg.topic << "\n";
+        std::cout << "Message: " << serverMsg.msg << "\n\n";
+    }
+
+    //Restore "ctrl + C" to normal functionality
+    action.sa_handler = SIG_DFL;
+    sigemptyset(&action.sa_mask);
+    sigaddset(&action.sa_mask, SIGINT);
+    action.sa_flags = 0;
+    sigaction(SIGINT, &action, NULL);
+}
+
+void endListen(int sig){
+    listening = false;
 }
--- Server.cc
+++ Server.cc
@@ -15,9 +15,8 @@
 #include "Topic.h"
 #include "Subscription.h"
 
-#define PORT 4212
+#define PORT 4211
 #define QUEUE_LEN 15
-#define MSGSIZE 128
 
 // std::string printSubs(std::vector<Subscription*> subs, bool printTopics){
 //     std::string print = "# ";
@@ -36,17 +35,20 @@
 //     return print;
 // }
 
-int main(){
-    // TopicManager topicMgr;    
-    // SubscriptionManager subMgr;
+
+/**
+ * Handles the opening and setup of the sockot the server will operate on and
+ * starts the server listening for connections
+ * @return the filedescriptor to the server's socket
+ */
+int startServerSocket(){
+    //setup info for the port the server should run on
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(PORT);
 
     int serverFd; 
-    int tempClientFd;
-    std::vector<int> clientFd; //ToDo: consider only using subscriptions to manage these.
-
-    struct sockaddr_in serverAddr;
-    struct sockaddr_in clientAddr;
-
     //Open Socket File Descriptor
     serverFd = socket(PF_INET, SOCK_STREAM, 0);
     if(serverFd == -1){
@@ -60,10 +62,6 @@ int main(){
         perror("Set reuse errors");
         exit(-1);
     }
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(PORT);
     
     //Bind server to localhost
     if(bind(serverFd, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == -1){
@@ -71,49 +69,130 @@ int main(){
         exit(-1);
     }
 
+    //Set server to listen for connections
     if(listen(serverFd, QUEUE_LEN) == -1){
         perror("Listen Error");
         exit(-1);
     }
 
+    return serverFd;
+}
+
+
+/**
+ * Process a response and take the appropriate action such as add the client as 
+ * a subscriber to a topic, publishing a topic, or disconnecting a client
+ * 
+ * @param socketFd a file descriptor of the socket that the response was from
+ * @param response a MsgOacket object that holds the response details
+ * @return 1 if the port is functioning properly. 0 if the fd has been closed or there is a port error
+ */
+int handleResponse(int socketFd, MsgPacket response){
+    printMsg(response);
+
+    if(response.type == MT_Connect){
+        MsgPacket msgPck;
+        msgPck.type = MT_Conn_ACK;
+        if(write(socketFd, &msgPck, sizeof(MsgPacket))==-1){perror("Write Failed"); return -1;}
+        return 1;
+    }
+    else if(response.type == MT_Disconnect){
+        MsgPacket msgPck;
+        msgPck.type = MT_Disc_ACK;
+        if(write(socketFd, &msgPck, sizeof(MsgPacket))==-1){perror("Write Failed"); return -1;}
+
+//TODO: Delete all subscriptions
+
+        close(socketFd);
+        return -1;
+    }
+    else if(response.type == MT_Publish){
+
+    }
+    else if(response.type == MT_Subscribe){
+
+    }
+
+    perror("Unknown Response");
+    return -1;
+    
+}
+
+
+/**
+ * Handles the read and response cycle for a single client
+ * 
+ * @param clientFd the file descriptor of the socket to handle
+ */
+void handleClient(int clientFd){
+    MsgPacket response;
+    int open = 1;
+    while(open == 1){
+        int readSize = read(clientFd, &response, sizeof(MsgPacket));
+        if(readSize == -1){perror("Write Failed");}
+
+        open = handleResponse(clientFd, response);
+
+    }
+}
+
+int main(){
+    // TopicManager topicMgr;    
+    // SubscriptionManager subMgr;
+
+    int serverFd; 
+    int tempClientFd;
+    std::vector<int> clientFd; //ToDo: consider only using subscriptions to manage these.
+
+    serverFd = startServerSocket();
+
+
     std::cout << "Start accepting client connections:\n";
+
+    struct sockaddr_in clientAddr;
     socklen_t clientAddrSize = sizeof(clientAddr);
 
     MsgPacket msgPck;
-    msgPck.type = MT_Conn_ACK;
-    msgPck.retain = true;
-    strcpy(msgPck.topic,"Test");
 
-    for(int i =0; i<3; i++){
+    for(int i =0; i<1; i++){
         tempClientFd = accept(serverFd, (struct sockaddr*) &clientAddr, &clientAddrSize);
         if(tempClientFd == -1){
             perror("Problem accepting connection");
             continue; //Jump to next loop and wait for next connection
         }
 
-        strcpy(msgPck.msg, "imessaghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhe");
-
         std::cout<<"Connection made: "<<inet_ntoa(clientAddr.sin_addr)<<"\n";
         
-        int wrote = write(tempClientFd, &msgPck, sizeof(MsgPacket));
-        if(wrote == -1){perror("Write Failed");}
-        
-        clientFd.push_back(tempClientFd);
-        std::cout << "Wrote: "<< wrote<<" of: "<<sizeof(msgPck)<<"\n";
+        //handleClient(tempClientFd);
+        //clientFd.push_back(tempClientFd);
+        int readSize = read(tempClientFd, &msgPck, sizeof(MsgPacket));
+        if(readSize == -1){perror("Write Failed");}
+
+        handleResponse(tempClientFd, msgPck);
+
+        msgPck.type = MT_Publish;
+        strcpy(msgPck.topic, "Test");
+        strcpy(msgPck.msg, "Hello World");
+
+        write(tempClientFd, &msgPck, sizeof(MsgPacket));
+
+        sleep(30);
+
+        msgPck.type = MT_Publish;
+        strcpy(msgPck.topic, "Test2");
+        strcpy(msgPck.msg, "Hello World");
+
+        write(tempClientFd, &msgPck, sizeof(MsgPacket));
+
+        readSize = read(tempClientFd, &msgPck, sizeof(MsgPacket));
+        if(readSize == -1){perror("Write Failed");}
+
+        handleResponse(tempClientFd, msgPck);
+
     }
 
-    msgPck.type = MT_Disconnect;
-    msgPck.retain = false;
-     strcpy(msgPck.topic, "");
-     strcpy(msgPck.msg, "bye");
 
-    for(int i = 0; i<clientFd.size(); i++){
-       if(write(clientFd.at(i), &msgPck, sizeof(MsgPacket)) == -1){perror("Write Failed");}
-        close(clientFd.at(i));
-    }
-
-    std::cout << "Goodbyes sent.\n";
-
+    std::cout << "Shutting Down.\n";
     close(serverFd);
 
 }
